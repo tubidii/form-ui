8bb0f6f58bd663a03a4545d6df92cdb0811f74c3
Daniel Waruo <waruodaniel@gmail.com>
2022-04-05 22:11:08 +0300

    feat: serve using express

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
+const path = require('path');
 const express = require('express');
-const cors = require('cors')
 const app = express();
-
-app.use(cors());
-
-app.use('/login', (req, res) => {
-  res.send({
-    token: 'test123'
-  });
+const publicPath = path.join(__dirname, 'build');
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
-
-app.listen(8000, () => console.log('API is running on http://localhost:8080/login'));
+app.listen(port, () => {
+  console.log(`Server is up! PORT = ${port}`);
+});
